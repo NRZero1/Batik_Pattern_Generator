50c51d90ce5bbac61ef9b0f67662ef3ce55f5cd3
NRZero1 <ronaldo.pangestu1@gmail.com>
2020-11-28 18:09:33 +0700

    Backend is finished

--- utama.pas
+++ utama.pas
@@ -26,6 +26,7 @@ type
     OpenPictureDialog2: TOpenPictureDialog;
     OpenPictureDialog3: TOpenPictureDialog;
     SavePictureDialog1: TSavePictureDialog;
+    ScrollBox1: TScrollBox;
     procedure btnGenerateClick(Sender: TObject);
     procedure btnPattern1Click(Sender: TObject);
     procedure btnPattern2Click(Sender: TObject);
@@ -33,7 +34,7 @@ type
     procedure btnTextureClick(Sender: TObject);
     procedure Grayscale(functionGray: Byte);
     procedure Biner1();
-    //procedure Biner2();
+    procedure Biner2();
     procedure Arithmetic();
     procedure LPF();
     procedure HPF();
@@ -61,22 +62,27 @@ uses
   windows;
 
 var
-  bitmapR4, bitmapG4, bitmapB4: array[0..1000, 0..1000] of Byte;
-  bitmapBiner3: array[0..1000, 0..1000] of Boolean;
-  bitmapBiner4: array[0..1000, 0..1000] of Boolean;
+  Arith:
+  record
+    bitmapBiner: array[0..1000, 0..1000] of Boolean;
+    bitmapBiner2: array[0..1000, 0..1000] of Boolean;
+    bitmapBiner3: array[0..1000, 0..1000] of Boolean;
+  end;
 
   FL:
   record
     bitmapR, bitmapG, bitmapB: array[0..1000, 0..1000] of Byte;
     gray: array[0..1000, 0..1000] of Byte;
-    gray2: array[0..1000, 0..1000] of Byte;
     bitmapBiner: array[0..1000, 0..1000] of Boolean;
     bitmapBiner2: array[0..1000, 0..1000] of Boolean;
+    bitmapBinerColor: array[0..1000, 0..1000] of Byte;
+    invers: array[0..1000, 0..1000] of Byte;
   end;
 
   Snow:
   record
     bitmapR, bitmapG, bitmapB: array[0..1000, 0..1000] of Byte;
+    invers: array[0..1000, 0..1000] of Byte;
   end;
 
   T:
@@ -125,7 +131,7 @@ var
 begin
   if OpenPictureDialog2.Execute then
   begin
-    Image2.Picture.LoadFromFile(OpenPictureDialog1.FileName);
+    Image2.Picture.LoadFromFile(OpenPictureDialog2.FileName);
     for y:=0 to Image2.Height-1 do
     begin
       for x:=0 to Image2.Width-1 do
@@ -144,7 +150,7 @@ var
 begin
   if OpenPictureDialog3.Execute then
   begin
-    Image3.Picture.LoadFromFile(OpenPictureDialog1.FileName);
+    Image3.Picture.LoadFromFile(OpenPictureDialog3.FileName);
     for y:=0 to Image3.Height-1 do
     begin
       for x:=0 to Image3.Width-1 do
@@ -165,14 +171,13 @@ procedure TForm1.Grayscale(functionGray: Byte);
 var
   x,y: Integer;
 begin
-  gray := 0;
   if functionGray = 1 then
   begin
     for y:=0 to Image1.Height-1 do
     begin
       for x:=0 to Image1.Width-1 do
       begin
-        FL.gray[x,y] := (bitmapR[x,y] + bitmapG[x,y] + bitmapB[x,y]) div 3;
+        FL.gray[x,y] := (FL.bitmapR[x,y] + FL.bitmapG[x,y] + FL.bitmapB[x,y]) div 3;
       end;
     end;
   end
@@ -183,7 +188,7 @@ begin
     begin
       for x:=0 to Image2.Width-1 do
       begin
-        DT.gray[x,y] := (bitmapR2[x,y] + bitmapG2[x,y] + bitmapB2[x,y]) div 3;
+        DT.gray[x,y] := (Snow.bitmapR[x,y] + Snow.bitmapG[x,y] + Snow.bitmapB[x,y]) div 3;
       end;
     end;
   end;
@@ -192,7 +197,8 @@ end;
 procedure TForm1.Biner1();
 var
   x, y: Integer;
-  functionCalls: Byte;
+  functionGray: Byte;
+  functionInvers: Byte;
 begin
   functionGray := 1;
   Grayscale(functionGray);
@@ -217,11 +223,11 @@ procedure TForm1.Biner2();
 var
   x, y: Integer;
 begin
-  for y:=0 to Image2.Height-1 do
+  for y:=0 to Image1.Height-1 do
   begin
-    for x:=0 to Image2.Width-1 do
+    for x:=0 to Image1.Width-1 do
     begin
-      if FL.gray2[x,y] > 165 then
+      if FL.gray[x,y] > 165 then
       begin
         FL.bitmapBiner2[x,y] := true;
       end
@@ -236,7 +242,6 @@ end;
 procedure TForm1.Invers(functionInvers: Byte);
 var
   x,y: Integer;
-  functionGray: Byte;
 begin
   if functionInvers = 1 then
   begin
@@ -244,29 +249,51 @@ begin
     begin
       for x:=0 to Image2.Width-1 do
       begin
-        DT.inversGray[x,y] := 255 - bitmapFilterGray[x, y];
-
-        if DT.inversGray[x,y] < 0 then
-        begin
-          DT.inversGray[x,y] := 0;
-        end;
-
-        if DT.inversGray[x,y] > 255 then
-        begin
-          DT.inversGray[x,y] := 255;
-        end;
+        DT.inversGray[x,y] := 255 - DT.bitmapFilterGray[x, y];
       end;
     end;
   end
 
   else if functionInvers = 2 then
   begin
-  end;
+    for y:=0 to Image2.Height-1 do
+    begin
+      for x:=0 to Image2.Width-1 do
+      begin
+        Snow.invers[x,y] := 255 - DT.inversGray[x,y];
+      end;
+    end;
+  end
 end;
 
 procedure TForm1.Arithmetic();
+var
+  x,y: Integer;
 begin
+  for y:=0 to Image1.Height-1 do
+  begin
+    for x:=0 to Image1.Width-1 do
+    begin
+      if Snow.invers[x,y] > 127 then
+      begin
+        Arith.bitmapBiner2[x,y] := true;
+      end
+      else
+      begin
+        Arith.bitmapBiner2[x,y] := false;
+      end;
+    end;
+  end;
 
+  for y:=0 to Image1.Height-1 do
+  begin
+    for x:=0 to Image1.Width-1 do
+    begin
+      Arith.bitmapBiner[x,y] := (not FL.bitmapBiner[x,y]) and Arith.bitmapBiner2[x,y];
+
+      Arith.bitmapBiner3[x,y] := Arith.bitmapBiner[x,y] or FL.bitmapBiner2[x,y];
+    end;
+  end;
 end;
 
 procedure TForm1.LPF();
@@ -363,11 +390,11 @@ end;
 procedure TForm1.HPF();
 var
   x,y: Integer;
-  kernel: array[1..3, 1..3] of Byte =
+  kernel: array[1..3, 1..3] of Integer =
   (
-    (1, 1, 1),
-    (1, 9, 1),
-    (1, 1, 1)
+    (-1, -1, -1),
+    (-1, 9, -1),
+    (-1, -1, -1)
   );
   i,j: Integer;
   index_x, index_y: Integer;
@@ -444,9 +471,9 @@ begin
         filterB := 255;
       end;
 
-      SH.bitmapFilterR[x,y] := filterR;
-      SH.bitmapFilterG[x,y] := filterG;
-      SH.bitmapFilterB[x,y] := filterB;
+      SH.bitmapFilterR[x,y] := Round(filterR);
+      SH.bitmapFilterG[x,y] := Round(filterG);
+      SH.bitmapFilterB[x,y] := Round(filterB);
     end;
   end;
 end;
@@ -454,32 +481,29 @@ end;
 procedure TForm1.EdgeD();
 var
   x,y: Integer;
-  kernel: array[1..3, 1..3] of Byte =
+  kernel: array[1..3, 1..3] of Integer =
   (
-    (1, 1, 1),
-    (1, 8, 1),
-    (1, 1, 1)
+    (-1, -1, -1),
+    (-1, 8, -1),
+    (-1, -1, -1)
   );
   i,j: Integer;
   index_x, index_y: Integer;
-  filterGray: Double;
+  filterGray: Integer;
   functionGray: Byte;
-  functionInvers: Byte;
 begin
-  functionGray: 2;
+  functionGray:= 2;
   Grayscale(functionGray);
 
   for y:=0 to Image2.Height-1 do
   begin
     for x:=0 to Image2.Width-1 do
     begin
-      filterR := 0;
-      filterG := 0;
-      filterB := 0;
+      filterGray := 0;
 
-      for i:=1 to 3 do
+      for j:=1 to 3 do
       begin
-        for j:=1 to 3 do
+        for i:=1 to 3 do
         begin
           index_x := x - (i - 2);
           index_y := y - (j - 2);
@@ -504,7 +528,7 @@ begin
             index_y := Image2.Height-1;
           end;
 
-          filterGray := filterGray + DT.gray[x,y] * kernel[i,j];
+          filterGray := filterGray + DT.gray[index_x, index_y] * kernel[i,j];
         end;
       end;
 
@@ -521,21 +545,19 @@ begin
       DT.bitmapFilterGray[x,y] := filterGray;
     end;
   end;
-  Invers(functionInvers);
+  Invers(1);
 end;
 
 procedure TForm1.Flower();
-var
-  functionInvers: Byte;
 begin
-  functionInvers := 2;
   Biner1();
-  Invers(functionInvers);
+  Biner2();
 end;
 
 procedure TForm1.Snowflake();
 begin
-  DT();
+  EdgeD();
+  Invers(2);
 end;
 
 procedure TForm1.Texture();
@@ -545,8 +567,30 @@ begin
 end;
 
 procedure TForm1.btnGenerateClick(Sender: TObject);
+var
+  x,y: Integer;
 begin
+  Flower();
+  Snowflake();
   Arithmetic();
+
+  Image4.Height := Image1.Height;
+  Image4.Width := Image1.Width;
+
+  for y:=0 to Image1.Height-1 do
+  begin
+    for x:=0 to Image1.Height-1 do
+    begin
+      if Arith.bitmapBiner3[x,y] then
+      begin
+        Image4.Canvas.Pixels[x,y] := RGB(T.bitmapR[x,y], T.bitmapG[x,y], T.bitmapB[x,y]);
+      end
+      else
+      begin
+        Image4.Canvas.Pixels[x,y] := RGB(0 ,0 ,0);
+      end;
+    end;
+  end;
 end;
 
 end.
